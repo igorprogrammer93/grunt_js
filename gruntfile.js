6412580fdbec3e9bc836b6d405ef630fe254f28b
igorprogrammer93 <igormtba@gmail.com>
2024-08-04 15:16:32 -0300

    commitminifi

--- gruntfile.js
+++ gruntfile.js
@@ -84,7 +84,15 @@ module.exports = function(grunt){
 
             }
         }, 
-        clean: ['prebuild']
+        clean: ['prebuild'],
+        uglify: {
+            target:{
+                files: {
+                    'dist/scripts/main.min.js': 'src/scripts/main.js'
+                }
+            }
+        }
+
     })
 
     grunt.loadNpmTasks('grunt-contrib-less');
@@ -92,8 +100,9 @@ module.exports = function(grunt){
     grunt.loadNpmTasks('grunt-replace');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     
 
     grunt.registerTask('default',['watch']);
-    grunt.registerTask('build',['less:production','htmlmin:dist','replace:dist', 'clean']);
+    grunt.registerTask('build',['less:production','htmlmin:dist','replace:dist', 'clean', 'uglify']);
 }
