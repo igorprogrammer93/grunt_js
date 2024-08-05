a84985a96cb912d62b110baabe1ab783ff532d9a
igorprogrammer93 <igormtba@gmail.com>
2024-08-05 20:23:08 -0300

    commit2

--- gruntfile.js
+++ gruntfile.js
@@ -84,7 +84,6 @@ module.exports = function(grunt){
 
             }
         }, 
-        clean: ['prebuild'],
         uglify: {
             target:{
                 files: {
@@ -99,10 +98,9 @@ module.exports = function(grunt){
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-replace');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
-    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     
 
     grunt.registerTask('default',['watch']);
-    grunt.registerTask('build',['less:production','htmlmin:dist','replace:dist', 'clean', 'uglify']);
+    grunt.registerTask('build',['less:production','htmlmin:dist','replace:dist', 'uglify']);
 }
